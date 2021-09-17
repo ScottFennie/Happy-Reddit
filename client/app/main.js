a11523c07b0e0bb53ec1251b8f892c8ab77f0038
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-17 13:39:14 -0600

    just getting back from lunch

--- client/app/main.js
+++ client/app/main.js
@@ -1,9 +1,11 @@
 import { AuthController } from './Controllers/AuthController.js'
 import { ValuesController } from './Controllers/ValuesController.js'
+import { SubraggitsController } from './Controllers/SubraggitsController.js'
 
 class App {
   authController = new AuthController();
   valuesController = new ValuesController();
+  subraggitsController = new SubraggitsController()
 }
 
 // @ts-ignore
