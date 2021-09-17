c88857107343afc8a5a940b8b1d1f86e91b5677b
Scott Fennie <s.fennie7@gmail.com>
2021-09-17 11:26:26 -0600

    subraggit get working

---
+++ server/models/subraggit.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+export const SubraggitSchema = new Schema({
+  subraggitId: { type: mongoose.Schema.Types.ObjectId },
+  title: { type: String, required: true, minlength: 1, maxlength: 25 },
+  creatorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Account' },
+  img: { type: String }
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+SubraggitSchema.virtual('creator', {
+  localField: 'creatorId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Account'
+})
