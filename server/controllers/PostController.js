2bb8d51cc3663395971330ca24df8c06a7cbca29
Scott Fennie <s.fennie7@gmail.com>
2021-09-17 15:25:19 -0600

    post controller stub

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -1,3 +1,5 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { postService } from '../services/PostsService'
 import BaseController from '../utils/BaseController'
 
 export class PostController extends BaseController {
@@ -5,6 +7,37 @@ export class PostController extends BaseController {
     super('api/posts')
     this.router
       .get('', this.getPosts)
+      .use(Auth0Provider.getAuthorizedUserInfo)
       .get('/:id', this.getPostById)
+      .post('', this.addPost)
+      .delete('/:id', this.deletePost)
+  }
+
+  deletePost(req, res, next) {
+    try {
+
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  addPost(req, res, next) {
+    try {
+
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  getPostById(arg0, getPostById) {
+    throw new Error('Method not implemented.')
+  }
+
+  async getPosts() {
+    try {
+      postService.getPosts()
+    } catch (error) {
+      next(error)
+    }
   }
 }
