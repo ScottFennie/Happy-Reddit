70d8c696a33b1af5ab96aae423fb4adf2bce0656
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-17 13:43:58 -0600

    updated main js client side

--- client/app/main.js
+++ client/app/main.js
@@ -1,11 +1,15 @@
 import { AuthController } from './Controllers/AuthController.js'
 import { ValuesController } from './Controllers/ValuesController.js'
 import { SubraggitsController } from './Controllers/SubraggitsController.js'
+import { PostsController } from './Controllers/PostsController.js'
+import { CommentsController } from './Controllers/CommentsController.js'
 
 class App {
   authController = new AuthController();
   valuesController = new ValuesController();
-  subraggitsController = new SubraggitsController()
+  subraggitsController = new SubraggitsController();
+  postsController = new PostsController();
+  commentsController = new CommentsController();
 }
 
 // @ts-ignore
